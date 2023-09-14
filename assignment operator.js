2f457acb21098d1a357355058fdba3177186b91c
YumanGong <gongymsilvia@gmail.com>
2023-09-14 10:33:18 -0500

    update code}

---
+++ assignment operator.js
@@ -0,0 +1,25 @@
+// count usage
+let num = 1;
+// num = num + 1;
+// 采取赋值运算符
+// num += 3;
+console.log(num);
+
+// 1.Prefix auto-increment
+// let i = 1;
+// ++i;   //
+// console.log(i);
+
+// let i = 1
+// console.log(++i + 1)
+
+
+// // 2.post-increment
+// let i = 1;
+// i++;   //
+// console.log(i);
+// let i = 1
+// console.log(i++ + 1)
+
+let i = 1
+console.log(i++ + ++i + i)
