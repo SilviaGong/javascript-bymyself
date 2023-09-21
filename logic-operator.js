111191b5295d69763787735b35339cb6096839f4
YumanGong <gongymsilvia@gmail.com>
2023-09-20 20:47:20 -0500

    update

--- logic-operator.js
+++ logic-operator.js
@@ -1,4 +1,10 @@
 //1. user input
-let num = +prompt('please enter a number:');//+ means convert the input information to number 
-//2.print out the result
-console.log(num % 4 === 0 && num % 100 !== 0);
+// let num = +prompt('please enter a number:');//+ means convert the input information to number 
+// //2.print out the result
+// console.log(num % 4 === 0 && num % 100 !== 0);
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+  console.log(phrase[i]);
+}
